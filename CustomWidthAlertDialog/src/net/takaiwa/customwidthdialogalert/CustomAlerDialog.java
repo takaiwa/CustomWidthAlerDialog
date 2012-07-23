c116c2e89462a22dbbfff8d213ff73d2d3c182e1
takaiwa29 <devtakaiwa@gmail.com>
2012-07-24 06:35:46 +0900

    first commit

---
+++ CustomWidthAlertDialog/src/net/takaiwa/customwidthdialogalert/CustomAlerDialog.java
@@ -0,0 +1,63 @@
+package net.takaiwa.customwidthdialogalert;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager.LayoutParams;
+
+public class CustomAlerDialog extends AlertDialog.Builder {
+
+    /**
+     * コンストラクタ
+     * ダイアログ側でサイズを指定
+     * @param context
+     */
+    public CustomAlerDialog(Context context) {
+        super(context);
+        this.showDialog(context, LayoutParams.FILL_PARENT);
+    }
+
+    /**
+     * コンストラクタ
+     * 呼び出し元に指定されたサイズでダイアログを表示
+     * @param context
+     * @param width
+     */
+    public CustomAlerDialog(Context context, int width) {
+        super(context);
+        this.showDialog(context,width);
+    }
+
+    private void showDialog(Context context, int width) {
+
+        // XMLファイルからレイアウトを読み込む
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.dialog_layout, null);
+
+        setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                /* OKボタンをクリックした時の処理 */
+                dialog.cancel();
+            }
+        });
+        setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                /* Cancelボタンをクリックした時の処理 */
+                dialog.cancel();
+            }
+        });
+//        setTitle("hogehoge");
+        setView(view);
+
+        Dialog d = create();
+        d.show();
+        LayoutParams lp = d.getWindow().getAttributes();
+        lp.width = width;
+        d.getWindow().setAttributes(lp);
+    }
+
+
+}
---
+++ CustomWidthAlertDialog/src/net/takaiwa/customwidthdialogalert/MainActivity.java
@@ -0,0 +1,36 @@
+package net.takaiwa.customwidthdialogalert;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.Display;
+import android.view.Menu;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // サイズ指定はダイアログ側に任せて表示する場合
+        new CustomAlerDialog(this);
+
+        // Activityでサイズを指定して表示する場合
+//        WindowManager wm = (WindowManager)getSystemService(Context.WINDOW_SERVICE);
+//        Display disp = wm.getDefaultDisplay();
+//        int width = (int)(disp.getWidth() * 0.8);
+//        // 画面幅80%で表示
+//        new CustomAlerDialog(this, width);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+
+}
